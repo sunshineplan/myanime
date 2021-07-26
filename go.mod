724e42e911e30b8ce0bbc5cb3c6183b52a191090
sunshineplan <sunshineplan@users.noreply.github.com>
2021-07-26 17:08:03 +0800

    Create dependabot.yml

--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.16
 require (
 	github.com/anaskhan96/soup v1.2.4
 	github.com/gin-gonic/gin v1.7.2
-	github.com/sunshineplan/gohttp v0.0.0-20210630011641-5b6cc891ec35
-	github.com/sunshineplan/service v0.0.0-20210625012150-c01e313a12f4
-	github.com/sunshineplan/utils v0.0.0-20210624055436-282dca1240dc
+	github.com/sunshineplan/gohttp v1.0.0
+	github.com/sunshineplan/service v1.0.1
+	github.com/sunshineplan/utils v0.0.1
 	github.com/vharitonsky/iniflags v0.0.0-20180513140207-a33cd0b5f3de
 )
--- main.go
+++ main.go
@@ -42,12 +42,9 @@ func init() {
 		log.Fatalln("Failed to get self path:", err)
 	}
 
-	agent, err := utils.UserAgentString()
-	if err != nil {
-		log.Print("Getting user agent failed. Use default agent instead.")
-		agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"
-	}
-	gohttp.SetAgent(agent)
+	gohttp.SetAgent(utils.UserAgent(
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+	))
 
 	rand.Seed(time.Now().UnixNano())
 }
